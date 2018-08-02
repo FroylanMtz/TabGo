9b0de21c919ee26692c2465899d801dd3895dad9
Unknown <1530006@upv.edu.mx>
2018-08-01 22:24:25 -0500

    Agregacion de la pagina de registro de clientes

---
+++ app/src/main/java/com/example/froylan/tabgo/Registro_clientes.java
@@ -0,0 +1,18 @@
+package com.example.froylan.tabgo;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+
+public class registro_clientes extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.registro);
+
+    }
+
+}
